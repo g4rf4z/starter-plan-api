5c916d5818fe400a8e277ece12c854db4593582e
Grégoire Pinte <gregoire.pinte@gmail.com>
2023-07-14 16:39:50 +0200

    Update product.schema.ts

--- src/schemas/product.schema.ts
+++ src/schemas/product.schema.ts
@@ -1,15 +1,13 @@
 import { number, object, string, TypeOf } from 'zod';
 
 export const createProductSchema = object({
-  params: object({}).optional(),
-  query: object({}).optional(),
   body: object({
     name: string(),
     url: string(),
     description: string(),
     price: number(),
   }).strict(),
-}).strict();
+});
 
 export type CreateProductInput = TypeOf<typeof createProductSchema>;
 
