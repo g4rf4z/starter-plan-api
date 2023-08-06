71c1d6d7aed585023c8cd6fecd2e07e1a2d7541d
Grégoire Pinte <gregoire.pinte@gmail.com>
2023-08-06 20:12:12 +0200

    Update user.database.ts

--- src/models/user/user.database.ts
+++ src/models/user/user.database.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 
-import { IUser, IUserFullPayload } from '@/models/user/user.entity';
+import { IUser, IUserFull, IUserFullPayload } from '@/models/user/user.entity';
 
 import { prisma } from '@/utils/prisma.util';
 
@@ -31,6 +31,25 @@ export class UserDatabase {
       return user;
     } catch (error) {
       throw console.error('UserDatabase.create', error);
+    }
+  }
+
+  async readById(id: IUserFull['id']): Promise<IUserFullPayload> {
+    try {
+      const user = await this.userDb.findUniqueOrThrow({
+        where: { id },
+        select: {
+          id: true,
+          createdAt: true,
+          updatedAt: true,
+          firstname: true,
+          lastname: true,
+          email: true,
+        },
+      });
+      return user;
+    } catch (error) {
+      throw console.error('UserDatabase.readById', error);
     }
   }
 }
