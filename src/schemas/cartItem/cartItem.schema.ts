cfe4fb9c5dfcd61e636f0dbc53339e1fb2b8bb4d
Grégoire Pinte <gregoire.pinte@gmail.com>
2023-08-19 14:13:54 +0200

    Update cartItem.schema.ts

--- src/schemas/cartItem/cartItem.schema.ts
+++ src/schemas/cartItem/cartItem.schema.ts
@@ -1,24 +1,23 @@
-import { array, object, string, TypeOf } from 'zod';
+import { number, object, string, TypeOf } from 'zod';
 
 export const createCartItemSchema = object({
   params: object({
     cartId: string(),
   }).strict(),
   body: object({
-    productId: array(string()),
+    productId: string(),
+    quantity: number(),
   }).strict(),
 });
-
 export type CreateCartItemInput = TypeOf<typeof createCartItemSchema>;
 
-export const readCartItemSchema = object({
-  body: object({
-    params: object({
-      cartId: string(),
-    })
-      .strict()
-      .optional(),
-  }).strict(),
-});
-
-export type ReadCartItemInput = TypeOf<typeof readCartItemSchema>;
+// export const readCartItemSchema = object({
+//   body: object({
+//     params: object({
+//       cartId: string(),
+//     })
+//       .strict()
+//       .optional(),
+//   }).strict(),
+// });
+// export type ReadCartItemInput = TypeOf<typeof readCartItemSchema>;
