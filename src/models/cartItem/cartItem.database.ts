04ed432a811caa269989a96c7643d95f8cf84f5e
Grégoire Pinte <gregoire.pinte@gmail.com>
2023-08-19 14:13:31 +0200

    Create cartItem.database.ts

---
+++ src/models/cartItem/cartItem.database.ts
@@ -0,0 +1,45 @@
+import { PrismaClient } from '@prisma/client';
+
+import { ICartItem, ICartItemFull } from '@/models/cartItem/cartItem.entity';
+
+import { prisma } from '@/utils/prisma.util';
+
+export class CartItemDatabase {
+  private cartItemDb: PrismaClient['cartItem'];
+
+  constructor() {
+    this.cartItemDb = prisma.cartItem;
+  }
+
+  // create cart item
+  async createCartItem({
+    cartId,
+    productId,
+    quantity,
+  }: ICartItem): Promise<ICartItemFull> {
+    try {
+      const cartItem = await this.cartItemDb.create({
+        data: {
+          cart: {
+            connect: {
+              id: cartId,
+            },
+          },
+          productId: productId,
+          quantity: quantity,
+        },
+        select: {
+          id: true,
+          createdAt: true,
+          updatedAt: true,
+          cartId: true,
+          productId: true,
+          quantity: true,
+        },
+      });
+      return cartItem;
+    } catch (error) {
+      throw console.error('CartItemDatabase.createCartItem', error);
+    }
+  }
+}
