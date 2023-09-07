14991476c83da4fdd5ac4f444322b6fed35c9b2b
Grégoire Pinte <gregoire.pinte@gmail.com>
2023-09-07 22:00:57 +0200

    Update createUser.schema.ts

--- src/schemas/user/createUser.schema.ts
+++ src/schemas/user/createUser.schema.ts
@@ -1,11 +1,19 @@
 import { object, string, TypeOf } from 'zod';
 
+import { validatePasswordComplexity } from '@/services/data.service';
+
 export const createUserSchema = object({
+  params: object({}).strict(),
   body: object({
     firstname: string(),
     lastname: string(),
-    email: string().email(),
+    email: string(),
     password: string(),
-  }).strict(),
+    passwordConfirmation: string(),
+  })
+    .strict()
+    .refine((data) => validatePasswordComplexity(data.password, 3))
+    .refine((data) => data.password === data.passwordConfirmation),
+  query: object({}).strict(),
 });
 export type CreateUserInput = TypeOf<typeof createUserSchema>;
