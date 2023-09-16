6eca35be1e4fb430deb12759dcd5eba8702c4149
Grégoire Pinte <gregoire.pinte@gmail.com>
2023-09-16 19:40:03 +0200

    Update requireAuthentication.middleware.ts

--- src/middlewares/requireAuthentication.middleware.ts
+++ src/middlewares/requireAuthentication.middleware.ts
@@ -5,7 +5,8 @@ export const requireAuthentication = (
   res: Response,
   next: NextFunction
 ) => {
-  console.log(res.locals);
+  console.log(res.locals.user);
+  console.log(res.locals.session);
   if (!res.locals?.user && !res.locals?.session) return res.sendStatus(403);
   return next();
 };
