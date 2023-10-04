5d59118734259036e455a976aa1b681ec6572c56
Grégoire Pinte <gregoire.pinte@gmail.com>
2023-10-04 22:44:37 +0200

    Update cart.entity.ts

--- src/models/cart/cart.entity.ts
+++ src/models/cart/cart.entity.ts
@@ -2,14 +2,15 @@ import { IUserFull } from '@/models/user/user.entity';
 import { ICartItem } from '@/models/cartItem/cartItem.entity';
 
 export interface ICart {
-  id?: string;
-  createdAt?: Date;
-  updatedAt?: Date;
+  id: string;
+  createdAt: Date;
+  updatedAt: Date;
   userId: IUserFull['id'];
   cartItems?: ICartItem[];
 }
 
 export type ICartFull = Required<ICart>;
+export type ICartReadById = Pick<ICart, 'id'>;
 export type ICartFullPayload = Omit<ICartFull, 'cartItems'>;
 
 export class Cart {
