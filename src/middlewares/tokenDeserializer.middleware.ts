f6db18e04af443b885160f34e4de410dac3f14cf
Grégoire Pinte <gregoire.pinte@gmail.com>
2023-10-04 22:41:06 +0200

    Update tokenDeserializer.middleware.ts

--- src/middlewares/tokenDeserializer.middleware.ts
+++ src/middlewares/tokenDeserializer.middleware.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 
-import { AccessToken } from '@/services/token.service';
 import { TokenService } from '@/services/token.service';
+import { AccessToken } from '@/services/token.service';
 
 export const tokenDeserializer = async (
   req: Request,
@@ -17,16 +17,14 @@ export const tokenDeserializer = async (
     res.locals.user = accessTokenData.user;
     res.locals.session = accessTokenData.session;
     return next();
-  } catch (error) {
-    //
-  }
+  } catch (error) {}
 
   try {
     const tokenService = new TokenService(true);
     const newAccessToken = await tokenService.reIssueAccessToken(refreshToken);
 
     res.cookie('accessToken', newAccessToken, {
-      maxAge: 900000, // 15 minutes
+      maxAge: 900000, // 15 minutes.
       httpOnly: true,
       sameSite: 'none',
       secure: true,
@@ -35,9 +33,6 @@ export const tokenDeserializer = async (
     const { user, session } = tokenService.verify<AccessToken>(newAccessToken);
     res.locals.user = user;
     res.locals.session = session;
-  } catch {
-    //
-  }
-
+  } catch {}
   return next();
 };
