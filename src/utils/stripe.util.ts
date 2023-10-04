b133f5a576140f3bc6d8bccf18c226d461273b65
Grégoire Pinte <gregoire.pinte@gmail.com>
2023-10-04 21:56:37 +0200

    Delete stripe.util.ts

--- src/utils/stripe.util.ts
+++
@@ -1,9 +0,0 @@
-import config from 'config';
-import Stripe from 'stripe';
-
-const stripeSecretKey = config.get<string>('stripeSecretKey');
-
-export const stripe = new Stripe(stripeSecretKey, {
-  apiVersion: '2023-08-16',
-  typescript: true,
-});
