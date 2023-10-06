4288494ddc0bf75361fe5a5f6416295aab557811
Grégoire Pinte <gregoire.pinte@gmail.com>
2023-10-06 14:09:50 +0200

    Update product.controller.ts

--- src/controllers/product/product.controller.ts
+++ src/controllers/product/product.controller.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 
 import { ReadProductInput } from '@/schemas/product/readProduct.schema';
 
