79058296ee777b9d4c77d79e423905836418eec1
Grégoire Pinte <gregoire.pinte@gmail.com>
2023-10-07 18:14:33 +0200

    Update createCartItem.schema.ts

--- src/schemas/cartItem/createCartItem.schema.ts
+++ src/schemas/cartItem/createCartItem.schema.ts
@@ -1,4 +1,4 @@
-import { number, object, string, TypeOf } from 'zod';
+import { object, string, number, TypeOf } from 'zod';
 
 export const createCartItemSchema = object({
   params: object({}).strict(),
