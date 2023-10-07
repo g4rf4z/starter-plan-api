7794488f451a328e457f2b5cd2e5f1903383fa00
Grégoire Pinte <gregoire.pinte@gmail.com>
2023-10-07 15:28:30 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -9,10 +9,21 @@ const apiUrl = config.get<string>('apiUrl');
 
 const app = createServer();
 
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
-
-app.listen(port, async () => {
-  console.info(`Server is running on ${apiUrl}:${port}.`);
+const server = app.listen(port, async () => {
+  console.info(`API is running on ${apiUrl}:${port}.`);
 });
+
+const shutdown = (signal: string) => {
+  return (err: any) => {
+    console.info(`${signal} received. API shutdown in progress...`);
+    server.close(() => {
+      console.info('API shutdown successful.');
+      process.exit(err ? 1 : 0);
+    });
+  };
+};
+
+process.on('SIGINT', shutdown('SIGINT'));
+process.on('SIGTERM', shutdown('SIGTERM'));
+process.on('uncaughtException', shutdown('uncaughtException'));
+process.on('unhandledRejection', shutdown('unhandledRejection'));
