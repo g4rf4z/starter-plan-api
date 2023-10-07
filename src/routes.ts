659faea72007a05a96defd835773b6b96b940415
Grégoire Pinte <gregoire.pinte@gmail.com>
2023-10-07 18:14:21 +0200

    Update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,48 +1,46 @@
 import { Express } from 'express';
 
-import { requireAuthentication } from '@/middlewares/requireAuthentication.middleware';
 import { validate } from '@/middlewares/validation.middleware';
+import { requireAuthentication } from '@/middlewares/requireAuthentication.middleware';
 
 import { createUserSchema } from '@/schemas/user/createUser.schema';
 import { readUserSchema } from '@/schemas/user/readUser.schema';
-
+import { loginSchema } from '@/schemas/session/login.schema';
+import { retrieveSessionSchema } from '@/schemas/session/retrieveSession.schema';
+import { logoutSchema } from '@/schemas/session/logout.schema';
+import { resetPasswordSchema } from '@/schemas/session/resetPasswordToken.schema';
+import { setNewPasswordSchema } from '@/schemas/session/setNewPassword.schema';
+import { readCartSchema } from '@/schemas/cart/readCart.schema';
 import { createCartItemSchema } from '@/schemas/cartItem/createCartItem.schema';
+import { updateCartItemSchema } from '@/schemas/cartItem/updateCartItem.schema';
+import { createPaymentIntentSchema } from '@/schemas/payment/paymentIntent.schema';
 
 import { createUserController } from '@/controllers/user/createUser.controller';
 import { readUserController } from '@/controllers/user/readUser.controller';
-
+import { loginController } from '@/controllers/session/login.controller';
+import { retrieveSessionController } from '@/controllers/session/retrieveSession.controller';
+import { logoutController } from '@/controllers/session/logout.controller';
+import { resetPasswordController } from '@/controllers/session/resetPasswordToken.controller';
+import { setNewPasswordController } from '@/controllers/session/setNewPassword.controller';
+import { readCartController } from '@/controllers/cart/readCart.controller';
 import { createCartItemController } from '@/controllers/cartItem/createCartItem.controller';
-
-import { createPaymentIntentSchema } from './schemas/payment/paymentIntent.schema';
-
+import { updateCartItemController } from '@/controllers/cartItem/updateCartItem.controller';
+import { findPublishableKeyController } from '@/controllers/payment/publishableKey.controller';
 import {
   createPaymentIntentController,
   findPaymentIntentsController,
-} from './controllers/payment/paymentIntent.controller';
+} from '@/controllers/payment/paymentIntent.controller';
 
-import { findPublishableKeyController } from './controllers/payment/publishableKey.controller';
-import { readCartSchema } from './schemas/cart/readCart.schema';
-import { readCartController } from './controllers/cart/readCart.controller';
-import { updateCartItemController } from './controllers/cartItem/updateCartItem.controller';
-import { updateCartItemSchema } from './schemas/cartItem/updateCartItem.schema';
-import { loginController } from './controllers/session/login.controller';
-import { loginSchema } from './schemas/session/login.schema';
-import { logoutController } from './controllers/session/logout.controller';
-import { logoutSchema } from './schemas/session/logout.schema';
-import { retrieveSessionController } from './controllers/session/retrieveSession.controller';
-import { retrieveSessionSchema } from './schemas/session/retrieveSession.schema';
-
-import { resetPasswordController } from './controllers/session/resetPasswordToken.controller';
-import { resetPasswordSchema } from './schemas/session/resetPasswordToken.schema';
-
-const routes = (app: Express) => {
+export const routes = (app: Express) => {
   app.get('/', (req, res) => {
     return res.send('Hello World !');
   });
 
-  // ---------- User routes ----------
+  // User route(s).
   app.post('/users', validate(createUserSchema), createUserController);
   app.get('/users', validate(readUserSchema), readUserController);
+
+  // Session route(s).
   app.post('/login', validate(loginSchema), loginController);
   app.post(
     '/logout',
@@ -52,7 +50,8 @@ const routes = (app: Express) => {
   );
   app.get(
     '/retrieve-session',
-    [requireAuthentication, validate(retrieveSessionSchema)],
+    requireAuthentication,
+    validate(retrieveSessionSchema),
     retrieveSessionController
   );
   app.post(
@@ -60,16 +59,16 @@ const routes = (app: Express) => {
     validate(resetPasswordSchema),
     resetPasswordController
   );
-  // app.post(
-  //   '/:userId/set-new-password/:token',
-  //   validate(setNewPasswordSchema),
-  //   setNewPasswordController
-  // );
+  app.post(
+    '/:userId/set-new-password/:token',
+    validate(setNewPasswordSchema),
+    setNewPasswordController
+  );
 
-  // ---------- Cart routes ----------
+  // Cart route(s).
   app.get('/carts/:userId', validate(readCartSchema), readCartController);
 
-  // ---------- Cart Item routes ----------
+  // Cart Item route(s).
   app.post(
     '/cart-items',
     validate(createCartItemSchema),
@@ -81,7 +80,7 @@ const routes = (app: Express) => {
     updateCartItemController
   );
 
-  // ---------- Stripe routes ----------
+  // Stripe route(s).
   app.post(
     '/create-payment-intent',
     validate(createPaymentIntentSchema),
@@ -90,5 +89,3 @@ const routes = (app: Express) => {
   app.get('/find-payment-intents', findPaymentIntentsController);
   app.get('/config', findPublishableKeyController);
 };
-
-export default routes;
