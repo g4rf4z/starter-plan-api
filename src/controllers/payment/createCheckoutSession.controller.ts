ac9d4d89dae5e59d40c40526eeb8d8052336df17
Grégoire Pinte <gregoire.pinte@gmail.com>
2023-10-13 11:37:05 +0200

    Update createCheckoutSession.controller.ts

--- src/controllers/payment/createCheckoutSession.controller.ts
+++ src/controllers/payment/createCheckoutSession.controller.ts
@@ -2,31 +2,38 @@ import { Request, Response, NextFunction } from 'express';
 
 import { stripe } from '@/services/stripe.service';
 
+import { createCheckoutSessionInput } from '@/schemas/payment/createCheckoutSession.schema';
+
 export const createCheckoutSessionController = async (
-  req: Request<{}, {}, {}>,
+  req: Request<{}, {}, createCheckoutSessionInput['body']>,
   res: Response,
   next: NextFunction
 ) => {
   try {
+    const { currency, productName, unitAmount, quantity } = req.body;
+
     const checkoutSession = await stripe.checkout.sessions.create({
       line_items: [
         {
           price_data: {
-            currency: 'eur',
+            currency: currency,
             product_data: {
-              name: 'T-shirt',
+              name: productName,
             },
-            unit_amount: 2500,
+            unit_amount: unitAmount,
           },
-          quantity: 1,
+          quantity: quantity,
         },
       ],
+      automatic_tax: {
+        enabled: true,
+      },
       mode: 'payment',
       success_url: 'http://localhost:3000/success',
       cancel_url: 'http://localhost:3000/cancel',
     });
     if (checkoutSession.url) {
-      return res.redirect(303, checkoutSession.url);
+      return res.send(303, checkoutSession.url);
     } else {
       return res
         .status(500)
