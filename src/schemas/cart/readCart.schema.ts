ea8f29498a6cc6c5090dc2915e7671b9f8658763
Grégoire Pinte <gregoire.pinte@gmail.com>
2023-10-15 13:07:44 +0200

    Update readCart.schema.ts

--- src/schemas/cart/readCart.schema.ts
+++ src/schemas/cart/readCart.schema.ts
@@ -2,7 +2,7 @@ import { object, string, TypeOf } from 'zod';
 
 export const readCartSchema = object({
   params: object({
-    userId: string(),
+    id: string(),
   }).strict(),
 });
 export type ReadCartInput = TypeOf<typeof readCartSchema>;
