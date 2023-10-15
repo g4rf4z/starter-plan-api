925f4348928ff891350bb772e031cbb5a062ba26
Grégoire Pinte <gregoire.pinte@gmail.com>
2023-10-15 10:09:16 +0200

    Improvement cartItem.entity

--- src/models/cartItem/cartItem.entity.ts
+++ src/models/cartItem/cartItem.entity.ts
@@ -1,32 +1,19 @@
-import { ICartFull } from '@/models/cart/cart.entity';
-import { IProductFull } from '@/models/product/product.entity';
+import { ICart } from '@/models/cart/cart.entity';
+import { IProduct } from '@/models/product/product.entity';
 
 export interface ICartItem {
   id?: string;
   createdAt?: Date;
   updatedAt?: Date;
-  cartId: ICartFull['id'];
-  productId: IProductFull['id'];
   quantity: number;
+  cartId: ICart['id'];
+  productId: IProduct['id'];
 }
 
+export type ICartItemCreate = Pick<
+  ICartItem,
+  'quantity' | 'cartId' | 'productId'
+>;
+export type ICartItemReadById = Pick<ICartItem, 'id'>;
 export type ICartItemUpdate = Pick<ICartItem, 'id' | 'quantity'>;
 export type ICartItemDelete = Pick<ICartItem, 'id'>;
-
-export class CartItem {
-  id;
-  createdAt;
-  updatedAt;
-  cartId;
-  productId;
-  quantity;
-
-  constructor(data: ICartItem) {
-    this.id = data.id;
-    this.createdAt = data.createdAt;
-    this.updatedAt = data.updatedAt;
-    this.cartId = data.cartId;
-    this.productId = data.productId;
-    this.quantity = data.quantity;
-  }
-}
