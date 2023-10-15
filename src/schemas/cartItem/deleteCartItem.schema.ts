734a4b41c94edefdd7ec89d8e3d4a58563556ad6
Grégoire Pinte <gregoire.pinte@gmail.com>
2023-10-15 10:10:02 +0200

    Update deleteCartItem.schema.ts

--- src/schemas/cartItem/deleteCartItem.schema.ts
+++ src/schemas/cartItem/deleteCartItem.schema.ts
@@ -1,10 +1,8 @@
-import { string, object, TypeOf } from 'zod';
+import { object, string, TypeOf } from 'zod';
 
 export const deleteCartItemSchema = object({
   params: object({
     id: string(),
   }).strict(),
-  body: object({}).strict(),
-  query: object({}).strict(),
 });
 export type DeleteCartItemInput = TypeOf<typeof deleteCartItemSchema>;
