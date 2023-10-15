04218f9aacfba5244dbb38b9a0165bffafbc42b0
Grégoire Pinte <gregoire.pinte@gmail.com>
2023-10-15 14:35:17 +0200

    Remove schema

--- src/schemas/user/deleteUser.schema.ts
+++
@@ -1,6 +0,0 @@
-import { object, TypeOf } from 'zod';
-
-export const deleteUserSchema = object({
-  params: object({}).strict(),
-});
-export type DeleteUserInput = TypeOf<typeof deleteUserSchema>;
--- src/schemas/user/readUser.schema.ts
+++
@@ -1,6 +0,0 @@
-import { object, TypeOf } from 'zod';
-
-export const readUserSchema = object({
-  params: object({}).strict(),
-});
-export type ReadUserInput = TypeOf<typeof readUserSchema>;
