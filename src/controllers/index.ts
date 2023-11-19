6f05757c970c74d8da6dee22f5b645de411d12c9
Grégoire Pinte <gregoire.pinte@gmail.com>
2023-11-19 22:45:17 +0100

    Add session to index.ts

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,3 +1,11 @@
+/* Session */
+import { loginController } from '@/controllers/session/login.controller';
+import { retrieveSessionController } from '@/controllers/session/retrieveSession.controller';
+import { logoutController } from '@/controllers/session/logout.controller';
+import { resetPasswordController } from '@/controllers/session/resetPasswordToken.controller';
+import { setNewPasswordController } from '@/controllers/session/setNewPassword.controller';
+
+/* User */
 import { createUserController } from '@/controllers/user/createUser.controller';
 import { readUserController } from '@/controllers/user/readUser.controller';
 import { updateUserController } from '@/controllers/user/updateUser.controller';
@@ -5,6 +13,14 @@ import { updateUserPasswordController } from '@/controllers/user/updateUserPassw
 import { deleteUserController } from '@/controllers/user/deleteUser.controller';
 
 export {
+  /* Session */
+  loginController,
+  retrieveSessionController,
+  logoutController,
+  resetPasswordController,
+  setNewPasswordController,
+
+  /* User */
   createUserController,
   readUserController,
   updateUserController,
