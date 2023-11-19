e0b64a67cca39b2de36c5a7723d250c04203c967
Grégoire Pinte <gregoire.pinte@gmail.com>
2023-11-19 09:33:50 +0100

    Update createUserProduct.controller.ts

--- src/controllers/userProduct/createUserProduct.controller.ts
+++ src/controllers/userProduct/createUserProduct.controller.ts
@@ -1,27 +1,27 @@
-import { Request, Response, NextFunction } from 'express';
+// import { Request, Response, NextFunction } from 'express';
 
-import { IUser } from '@/models/user/user.entity';
+// import { IUser } from '@/models/user/user.entity';
 
-import { ProductDatabase } from '@/models/product/product.database';
-import { UserProductDatabase } from '@/models/userProduct/userProduct.database';
+// import { ProductDatabase } from '@/models/product/product.database';
+// import { UserProductDatabase } from '@/models/userProduct/userProduct.database';
 
-export const createUserProductController = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const userId = res.locals.user.id as IUser['id'];
+// export const createUserProductController = async (
+//   req: Request,
+//   res: Response,
+//   next: NextFunction
+// ) => {
+//   try {
+//     const userId = res.locals.user.id as IUser['id'];
 
-    const productDb = new ProductDatabase();
-    const userProductDb = new UserProductDatabase();
+//     const productDb = new ProductDatabase();
+//     const userProductDb = new UserProductDatabase();
 
-    const userProduct = await userProductDb.create({
-      userId,
-      productId,
-    });
-    return res.status(201).json({ userProduct });
-  } catch (error) {
-    return next(error);
-  }
-};
+//     const userProduct = await userProductDb.create({
+//       userId,
+//       productId,
+//     });
+//     return res.status(201).json({ userProduct });
+//   } catch (error) {
+//     return next(error);
+//   }
+// };
