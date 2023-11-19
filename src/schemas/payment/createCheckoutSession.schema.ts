1361e4a054334e56879806e087c71dfb4efcbdab
Grégoire Pinte <gregoire.pinte@gmail.com>
2023-11-19 09:56:32 +0100

    Update createCheckoutSession.schema.ts

--- src/schemas/payment/createCheckoutSession.schema.ts
+++ src/schemas/payment/createCheckoutSession.schema.ts
@@ -1,13 +1,22 @@
-import { object, array, string, number, TypeOf } from 'zod';
+import { object, array, string, number, boolean, TypeOf } from 'zod';
 
 export const createCheckoutSessionSchema = object({
   body: object({
+    client_reference_id: string(),
     line_items: array(
       object({
         price: string(),
-        quantity: number(),
+        quantity: number().min(1),
       })
     ),
+    billing_address_collection: string(),
+    mode: string(),
+    automatic_tax: object({
+      enabled: boolean(),
+    }),
+    metadata: object({
+      purchased_product_ids: array(string()),
+    }),
   }).strict(),
 });
 export type createCheckoutSessionInput = TypeOf<
