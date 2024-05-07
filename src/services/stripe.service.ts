dd373b1030cc8178153840ceff00798a9294ad74
Grégoire Pinte <gregoire.pinte@gmail.com>
2024-05-08 01:27:45 +0200

    Update stripe.service.ts

--- src/services/stripe.service.ts
+++ src/services/stripe.service.ts
@@ -4,6 +4,6 @@ import Stripe from 'stripe';
 const stripeSecretKey = config.get<string>('stripeSecretKey');
 
 export const stripe = new Stripe(stripeSecretKey, {
-  apiVersion: '2023-10-16',
+  apiVersion: '2024-04-10',
   typescript: true,
 });
